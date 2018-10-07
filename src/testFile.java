64dbba664760c7bc4bde961f3894dea1f50b039b
wchen1325 <wchen1325@bths.edu>
2018-10-07 17:47:03 -0400

    OK ok

--- src/testFile.java
+++ src/testFile.java
@@ -4,7 +4,7 @@ public class testFile {
         //System.out.println("Hello Method Madness");
         //System.out.println(WChenLib.isPalindrome("racecar"));
         //System.out.println(WChenLib.datestr("10/19/1999"));
-        System.out.println(WChenLib.isFibonnaci(55));
+        System.out.println(WChenLib.isFibonnaci(56));
         //WChenLib.multiplicationtable(3,9);
     }
 }
