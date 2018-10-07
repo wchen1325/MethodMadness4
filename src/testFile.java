94feb66866496b0a2fd74ccedbb703981baea4b4
wchen1325 <wchen1325@bths.edu>
2018-10-07 18:19:51 -0400

    OK ok

--- src/testFile.java
+++ src/testFile.java
@@ -1,12 +1,12 @@
 public class testFile {
     public static void main (String[] args)
     {
-        //System.out.println("Hello Method Madness");
-        //System.out.println(WChenLib.isPalindrome("racecar"));
-        //System.out.println(WChenLib.datestr("10/19/1999"));
-        //System.out.println(WChenLib.isFibonnaci(56));
-        //System.out.println(WChenLib.isSequenceFibonnaci(2141553));
-        //.multiplicationtable(3,9);
+        System.out.println("Hello Method Madness");
+        System.out.println(WChenLib.isPalindrome("racecar"));
+        System.out.println(WChenLib.datestr("10/19/1999"));
+        System.out.println(WChenLib.isFibonnaci(56));
+        System.out.println(WChenLib.isSequenceFibonnaci(2141553));
+        WChenLib.multiplicationtable(3,9);
         System.out.println(WChenLib.leastCommonMultiple(50,23,13));
     }
 }
