94dbd9c343130a09484e96948e819c361ef05593
wchen1325 <wchen1325@bths.edu>
2018-10-07 17:12:51 -0400

    10/7/18

--- src/WChenLib.java
+++ src/WChenLib.java
@@ -13,4 +13,21 @@ public class WChenLib {
         String year = date.substring(6,10);
         return day + "-" + month + "-" + year;
     }
+
+
+
+    public static boolean isFibonnaci (int n){
+        String Strn = String.valueOf(n);
+        int num1 = Integer.valueOf(Strn.substring(0,1));
+        int num2 = Integer.valueOf(Strn.substring(1,2));
+        String Fnum = String.valueOf(num1) + String.valueOf(num2);
+        while(int i < String.valueOf(n).length()){
+            int num3 = num1 + num2;
+            Fnum = Fnum + String.valueOf(num3);
+            num1 = num2;
+            num2 = num3;
+            i = Fnum.length();
+        }
+        return (Integer.valueOf(Fnum) == n)
+    }
 }
--- src/testFile.java
+++ src/testFile.java
@@ -1,8 +1,9 @@
 public class testFile {
     public static void main (String[] args)
     {
-        System.out.println("Hello Method Madness");
-        System.out.println(WChenLib.isPalindrome("racecar"));
-        System.out.println(WChenLib.datestr("10/19/1999"));
+        //System.out.println("Hello Method Madness");
+        //System.out.println(WChenLib.isPalindrome("racecar"));
+        //System.out.println(WChenLib.datestr("10/19/1999"));
+        System.out.println(WChenLib.isFibonnaci(97897923));
     }
 }
