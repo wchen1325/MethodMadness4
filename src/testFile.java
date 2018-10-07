a9abb55d1f3c5ceb7f27b2136e1a704377df4e64
wchen1325 <wchen1325@bths.edu>
2018-10-07 18:54:29 -0400

    OK ok

--- src/testFile.java
+++ src/testFile.java
@@ -2,8 +2,9 @@ public class testFile {
     public static void main (String[] args)
     {
         System.out.println("Hello Method Madness");
-        System.out.println(WChenLib.isPalindrome("racecar"));
-        System.out.println(WChenLib.datestr("10/19/1999"));
+        //Methods
+        System.out.println(WChenLib.isPalindrome("racecar")); //Input a String word to check if it is a palindrome.
+        System.out.println(WChenLib.datestr("10/19/1999")); //reformatting mm/dd/yyyy to dd - mm - yyyy.
         System.out.println(WChenLib.isFibonnaci(56));
         System.out.println(WChenLib.isSequenceFibonnaci(2141553));
         WChenLib.multiplicationtable(3,9);
