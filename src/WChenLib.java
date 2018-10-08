f8a6d22773224d68e7e2203267ab41f3d6e61bab
wchen1325 <wchen1325@bths.edu>
2018-10-07 22:48:06 -0400

    OK ok

--- src/WChenLib.java
+++ src/WChenLib.java
@@ -2,45 +2,30 @@ public class WChenLib {
     public static boolean isPalindrome(String word){
         String Palindrome = "";
         for ( int i = 0; i <= word.length()-1; i++){
-            Palindrome = word.substring (i, i+1) + Palindrome;
+            Palindrome = word.substring (i, i+1) + Palindrome; //Creating a new String by adding letters backwards one by one.
         }
-        return (Palindrome.equals(word));
+        return (Palindrome.equals(word)); //return true if the new string created is the same as the string given.
     }
 
     public static String datestr(String date){
-        String month = date.substring(0,2);
+        String month = date.substring(0,2); //isolate month, day, and year into individual strings.
         String day = date.substring(3,5);
         String year = date.substring(6,10);
-        return day + "-" + month + "-" + year;
+        return day + "-" + month + "-" + year;// return the strings in the new format.
     }
 
-    public static boolean isSequenceFibonnaci (int n){
-        String Strn = String.valueOf(n);
-        int num1 = Integer.valueOf(Strn.substring(0,1));
-        int num2 = Integer.valueOf(Strn.substring(1,2));
-        String Fnum = String.valueOf(num1) + String.valueOf(num2);
-        int i = 0;
-        while( i < String.valueOf(n).length()){
-            int num3 = num1 + num2;
-            Fnum = Fnum + String.valueOf(num3);
-            num1 = num2;
-            num2 = num3;
-            i = Fnum.length();
-        }
-        return (Integer.valueOf(Fnum) == n);
-    }
 
     public static boolean isFibonnaci (int n){
         int num1 = 0;
         int num2 = 1;
         int num3 = 1;
-        if (n == num1 || n == num2){
+        if (n == num1 || n == num2){ //check to see if the number is 0 or 1;
             return true;
         }
         else{
-            while(num3 < n){
+            while(num3 < n){ //creating the Fibonnaci number sequence until the the next number is greater than n.
                  num3 = num1 + num2;
-                if (num3 == n){
+                if (num3 == n){ //comparing n with each number in the sequence.
                     return true;
                 }
                 num1 = num2;
@@ -51,7 +36,7 @@ public class WChenLib {
     }
 
     public static void multiplicationtable(int base, int range){
-         for(int n=0; n <= range; n++){
+         for(int n=0; n <= range; n++){ //loop until the base has multiplied all integers from 0 to the range given.
              System.out.println(base*n);
          }
     }
@@ -60,7 +45,7 @@ public class WChenLib {
         int num1M = num1;
         int num2M = num2;
         int num3M = num3;
-        while (num1 != num2 || num2 != num3){
+        while (num1 != num2 || num2 != num3){ //only the lower numbers will add itself until all numbers all equal.
             if (num1 < num2 || num1 < num3){
                 num1 += num1M;
             }
@@ -71,6 +56,6 @@ public class WChenLib {
                 num3 += num3M;
             }
         }
-        return num1;
+        return num1; //return any of the three number since all numbers are equal.
     }
 }
--- src/testFile.java
+++ src/testFile.java
@@ -5,9 +5,8 @@ public class testFile {
         //Methods
         System.out.println(WChenLib.isPalindrome("racecar")); //Input a String word to check if it is a palindrome.
         System.out.println(WChenLib.datestr("10/19/1999")); //reformatting mm/dd/yyyy to dd - mm - yyyy.
-        System.out.println(WChenLib.isFibonnaci(56));
-        System.out.println(WChenLib.isSequenceFibonnaci(2141553));
-        WChenLib.multiplicationtable(3,9);
-        System.out.println(WChenLib.leastCommonMultiple(50,23,13));
+        System.out.println(WChenLib.isFibonnaci(56)); //Check if a number is in the Fibonnaci sequence.
+        WChenLib.multiplicationtable(3,9); //Find multiples of a number up to a cap.
+        System.out.println(WChenLib.leastCommonMultiple(50,23,13)); //Find the least common multiple of three numbers.
     }
 }
